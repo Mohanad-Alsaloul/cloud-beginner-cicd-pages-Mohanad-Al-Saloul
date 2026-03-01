6406f426790b92e08aa8af12c99aa941f924665a
Mohanad-Al-Saloul <mohanndeasalol@gmail.com>
2026-03-01 22:59:53 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("btn").addEventListener("click", () => {
-    const msgElement = document.getElementById("msg");
-    msgElement.innerText = "Hello Welcome! OK";
-    
-    msgElement.className = "success-msg";
-});
